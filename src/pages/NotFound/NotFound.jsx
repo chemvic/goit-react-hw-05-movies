14015daf423f8d040c5789362cf024cdadca7115
chemvic <chemvic79@gmail.com>
2023-05-06 16:55:23 +0300

    стили

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,7 @@
+import css from "./NotFound.module.css";
+
 const NotFound=()=>{
-    return (<div>
+    return (<div className={css.massege}>
       <h1>Page on this path not found</h1> 
     </div>)
 };
